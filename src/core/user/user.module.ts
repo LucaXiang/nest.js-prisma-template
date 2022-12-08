5f77de923ebc16b7daa647dd3152d3092c1c6d46
Luca Xiang <luca.xiang@outlook.com>
2022-12-08 21:17:27 +0100

    feat: import user controller and user service

--- src/core/user/user.module.ts
+++ src/core/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@prisma/prisma.module';
+import { UserService } from '@core/user/user.service';
+import { UserController } from '@core/user/user.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
