97428cc0b1e9fea57da729fe4a40e393cf851b05
Luca Xiang <luca.xiang@outlook.com>
2022-12-08 23:48:18 +0100

    feat: add jwt strategy

---
+++ src/common/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserPayload } from '@core/auth/types/user.payload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  public validate(payload: UserPayload): UserPayload {
+    return payload;
+  }
+}
