4e0ccfb32b49fd60cd7eac194d68108e29c659d9
Luca Xiang <luca.xiang@outlook.com>
2022-12-08 21:20:02 +0100

    feat: import auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from '@core/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
