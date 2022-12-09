45d16b003ebb53f08e3e544ed16214a7c77c9a93
Luca Xiang <luca.xiang@outlook.com>
2022-12-09 01:17:30 +0100

    chore: remove unused package

--- src/common/strategies/jwt.strategy.ts
+++ src/common/strategies/jwt.strategy.ts
@@ -2,9 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { UserPayload } from '@core/auth/types/user.payload';
-import * as dotenv from 'dotenv';
-import * as process from 'process';
-dotenv.config();
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
