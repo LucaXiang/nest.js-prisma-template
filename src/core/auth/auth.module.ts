da5801cb652a4b2e52052facd739fa1b747bafe7
Luca Xiang <luca.xiang@outlook.com>
2022-12-09 01:20:22 +0100

    feat: import user module, and fix jwt module option

--- src/core/auth/auth.module.ts
+++ src/core/auth/auth.module.ts
@@ -5,12 +5,14 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from '@core/auth/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from '@strategies/jwt.strategy';
+import { UserModule } from '@core/user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     PassportModule,
     JwtModule.register({
-      secretOrPrivateKey: process.env.JWT_SECRET,
+      secret: process.env.JWT_SECRET,
     }),
   ],
   controllers: [AuthController],
