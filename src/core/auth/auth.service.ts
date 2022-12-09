c9d76c57742ed2ca5ab2c00a4b955ece439adb03
Luca Xiang <luca.xiang@outlook.com>
2022-12-09 01:19:48 +0100

    feat: support login other user (no admin)

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { UserPayload } from '@core/auth/types/user.payload';
 import { JwtService } from '@nestjs/jwt';
+import { UserService } from '@core/user/user.service';
+import { compare } from '@utils/hash';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
-  validateUser(username: string, password: string): UserPayload {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<UserPayload> {
     let payload: UserPayload = null;
     do {
-      console.log(username, password);
       if (username == 'admin' && password == process.env.ADMIN_PASSWORD) {
         payload = {
           username: 'admin',
           role: 'admin',
         };
+        break;
+      }
+      const user = await this.userService.findOneWithUsername(username);
+      if (user != null) {
+        if (compare(password, user.password)) {
+          payload = {
+            username,
+            role: user.role,
+          };
+        }
         break;
       }
       // user service
