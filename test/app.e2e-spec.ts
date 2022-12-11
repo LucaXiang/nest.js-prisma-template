8d64097cb7b1659e3c6dd56dd1fa218e57778620
Luca Xiang <luca.xiang@outlook.com>
2022-12-11 02:57:51 +0100

    test: auth test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,11 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app/app.module';
+import { config } from 'dotenv';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let access_token: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    config();
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -20,5 +23,34 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/auth/login (POST)', () => {
+    const login_user = {
+      username: 'admin',
+      password: process.env.ADMIN_PASSWORD,
+    };
+
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send(login_user)
+      .expect(201)
+      .then((res) => {
+        access_token = JSON.parse(res.text).access_token;
+      });
+  });
+
+  it('/auth/profile (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/auth/profile')
+      .auth(access_token, {
+        type: 'bearer',
+      })
+      .expect(200)
+      .expect('Hello admin');
+  });
+
+  beforeAll(async () => {
+    await app.close();
   });
 });
