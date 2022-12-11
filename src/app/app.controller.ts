3b76a5c346ed22152a2171af06c52169232fade2
Luca Xiang <luca.xiang@outlook.com>
2022-12-11 16:21:22 +0100

    chore: remove test code

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { InjectRedis } from '@liaoliaots/nestjs-redis';
-import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @InjectRedis() private readonly redisService: Redis,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello() {
-    await this.redisService.set('hello', 'world');
-    return this.redisService.get('hello');
+    return this.appService.getHello();
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@Prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly prismaService: PrismaService) {}
-
-  findAll() {
-    return this.prismaService.user.findMany();
-  }
-
   getHello(): string {
     return 'Hello World!';
   }
